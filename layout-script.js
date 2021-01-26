e885365986e5b6b038b09fad708090dc95534f10
Saint4script <saint.4.script@gmail.com>
2021-01-27 01:57:58 +0800

    nothing good, still trying

--- layout-script.js
+++ layout-script.js
@@ -4,7 +4,10 @@ CABINETS = []
 let CHECKPOINTS = []
 
 class Cabinet {
-    destinationChecker = false
+    static destinationChecker = false;
+    static personToMove;
+    static placeholderToMoveFrom;
+    static personEvent;
 
     constructor(cab, coeffs) {
         this.cab = cab;
@@ -49,10 +52,6 @@ function resizeCabs() {
     }
 }
 
-$('.checkpoint').click((event) => {
-    console.log("f")
-});
-
 // input: HTML-element, HTML-element
 function getDistance(obj1, obj2) {
         let objSize = obj1.getBoundingClientRect();
@@ -79,37 +78,94 @@ function getClosestCheckpoint() {
     return closestCheckpoint;
 }
 
-function move(event, destination) {
-    event.stopPropagation(false);// как избавиться от этого?
-    let passedCheckpoints = []
 
-    // defining start checkpoint
-    let placeholderName = destination.classList[0];
+function move(who, from, to) {
+    who.stopPropagation(false);// как избавиться от этого?
+    let passedCheckpoints = [];
+        
+    function frame(obj, toX, toY) {
+        let me = $(obj.target)[0];
+        if (me.offsetLeft == toX && me.offsetTop == toY) {
+            clearInterval(time);
+        } else {
+            $(obj.target).offset ( {
+                left: me.offsetLeft + 1,
+                top: me.offsetTop + 1
+            });
+        }
+    }
+    // defining first checkpoint
+    let placeholderName = to.classList[0];
     let placeholderNumber = placeholderName.split('-')[1];
     let startCheckpoint;
-    let startCheckpointName;
+    // let startCheckpointName;
 
+    // getting first checkpoint
     for(let i = 0; i < CHECKPOINTS.length; i++) {
         if(Number(CHECKPOINTS[i].classList[0].split('-')[1]) == Number(placeholderNumber)) {
             startCheckpoint = CHECKPOINTS[i];
-            console.log(startCheckpoint);
             break;
         }
     }
+    //get last checkpoint !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
-    let objSizeNext = obj2.getBoundingClientRect();
-    
-    let me = event.currentTarget;
+    passedCheckpoints.push(startCheckpoint);
+    let x = $(who.target)[0].offsetLeft;
+    let y = $(who.target)[0].offsetTop;
+    let JQ_who = $(who.target).detach();
 
-    let size = me.getBoundingClientRect();
-    let stepX = 50;
-    let stepY = 50;
+    JQ_who.appendTo($('.floor-4-wrapper'));
+    JQ_who.offset ( {
+        left: x,
+        top: y
+    });
 
-    size.x += stepX;
-    size.y += stepY;
-    me.style+=`position: absolute; ; transition: 1s ease-out; transform: translate(${size.x}px, ${size.y}px)`;
+    console.log(CHECKPOINTS)
 
-    event.stopPropagation(true);
+    for(let i = 0; i < CHECKPOINTS.length; i++) {
+
+        let nextCoordinates = CHECKPOINTS[i].getBoundingClientRect();
+
+        // let tmpX = (nextCoordinates.x - $(who.target)[0].offsetLeft) / 100;
+        // let tmpY = (nextCoordinates.y - $(who.target)[0].offsetTop) / 100;
+        // let time = setInterval(() => {
+        //     if ($(who.target)[0].offsetLeft == nextCoordinates.x && $(who.target)[0].offsetTop == nextCoordinates.y) {
+        //             clearInterval(time);
+        //     } else {
+                
+        //         x = $(who.target)[0].offsetLeft + tmpX;
+        //         y = $(who.target)[0].offsetTop + tmpY;
+                // $(who.target).offset ( {
+                //     left: x,
+                //     top: y
+                // });
+                
+        //     }
+        // }, 1000);
+
+
+        let tmpX = (nextCoordinates.x - $(who.target)[0].offsetLeft);
+        let tmpY = (nextCoordinates.y - $(who.target)[0].offsetTop);
+        // console.log("$(who.target)[0].offsetLeft: " + $(who.target)[0].offsetLeft + "$(who.target)[0].offsetTop: " + $(who.target)[0].offsetTop);
+        // console.log("nextCoordinates.x: " + nextCoordinates.x + "nextCoordinates.y: " + nextCoordinates.y);
+        // console.log("tmpX: " + tmpX + "tmpY: " + tmpY);
+        
+        setTimeout(() => {
+            JQ_who.style+=`position: absolute; transition: 1s ease-out; transform: translate(${tmpX}px, ${tmpY}px)`;
+        }, 1000);
+        setTimeout(() => {
+            $(who.target).offset ( {
+                left: nextCoordinates.x,
+                top: nextCoordinates.y
+            });
+        }, 1000);
+        JQ_who.style+=`position: absolute; transition: 10s ease-out; transform: translate(0px, 0px)`;
+        
+        
+    }
+
+    who.stopPropagation(true);
+    Cabinet.to = false;
 }
 
 function getCabFromPlaceholder(placeholder) {
@@ -130,42 +186,68 @@ function getCabFromPlaceholder(placeholder) {
 }
 
 $('.placeholder').click((event) => {
+    let curEvent = event;
     let curPlace = event.currentTarget;
-    if(!Cabinet.destinationChecker) {
-        curPlace = event.currentTarget;
-        // condition for KORIDOR PARADNAYA PRIHOZHAYA
-        if(curPlace.classList[0] == "cab-1-1-place" || curPlace.classList[0] == "cab-1-2-place") {
-      
-            let child1 = $('.cab-1-1-place')[0];
-            let child2 = $('.cab-1-2-place')[0];
-        
-            let person = document.createElement('div');
-            person.setAttribute("class", "person-icon");
-        
-            if(child1.children.length >= child2.children.length){
-                child2.appendChild(person);
-            } else {
-                child1.appendChild(person);
-            }
-      
+    // console.log(Cabinet.destinationChecker)
+
+    // curPlace = event.currentTarget;
+    // condition for KORIDOR PARADNAYA PRIHOZHAYA
+    if(curPlace.classList[0] == "cab-1-1-place" || curPlace.classList[0] == "cab-1-2-place") {
+    
+        let child1 = $('.cab-1-1-place')[0];
+        let child2 = $('.cab-1-2-place')[0];
+    
+        let person = document.createElement('div');
+        person.setAttribute("class", "person-icon");
+    
+        if(child1.children.length >= child2.children.length){
+            child2.appendChild(person);
         } else {
-            let person = document.createElement('div');
-            person.setAttribute("class", "person-icon");
-        
-            curPlace.appendChild(person);
+            child1.appendChild(person);
         }
+    
     } else {
-        $(".person-icon").on("click", (event) => {
-            move(event, curPlace);
-            Cabinet.destinationChecker = true;
-        });
+        let person = document.createElement('div');
+        person.setAttribute("class", "person-icon");
+    
+        curPlace.appendChild(person);
     }
+
     $(".person-icon").on("click", (event) => {
-        Cabinet.destinationChecker = true;
+        Cabinet.personToMove = event;
+        Cabinet.placeholderToMoveFrom = curEvent;
     });
 });
 
-
+$(".move").on("click", (event) => {
+    // Cabinet.destinationChecker = true;
+    // if(Cabinet.destinationChecker) {
+    //     move(event, curPlace);
+    //     Cabinet.destinationChecker = false;
+    // }
+    let destinationPlaceholderName;
+    let destinationPlaceholder;
+    if(Cabinet.personToMove.currentTarget) {
+        let we = $("input[type='radio'");
+        for(let i = 0; i < we.length; i++) {
+            if(we[i].checked) {
+                destinationPlaceholderName = we[i].value;
+            }
+        }
+        
+        if(destinationPlaceholderName[6] == '-') {
+            destinationPlaceholder = $(`.${destinationPlaceholderName.slice(0, 7)}` + "place")[0];
+        } else {
+            destinationPlaceholder = $(`.${destinationPlaceholderName.slice(0, 6)}` + "place")[0];
+        }
+        
+        move(
+            Cabinet.personToMove,
+            Cabinet.placeholderToMoveFrom,
+            destinationPlaceholder
+            )
+    }
+});
 
 $(document).ready(() => {
     initCheckpoints();
